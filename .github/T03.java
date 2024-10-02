a1838a14a3fef39975878eb24f23eb26711f9067
Maxwell Rumahorbo <maxrumbo06@gmail.com>
2024-10-02 16:55:10 -0700

    ,,,,

--- .github/T03.java
+++ .github/T03.java
@@ -1,10 +1,7 @@
-//12S24037-Maxwell Rumahorbo
-//12S24030-Swasti Sihombing
 import java.util.*;
 import java.lang.Math;
 
-
-public class T03 {
+public class JavaApplication {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
