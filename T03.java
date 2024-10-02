db536e1c4bfcc1dca6ca53d66a31f342c73e2440
Maxwell Rumahorbo <maxrumbo06@gmail.com>
2024-10-02 15:43:50 -0700

    SUCCESS

--- T03.java
+++ T03.java
@@ -1,1 +1,69 @@
-// NIM - Your Name
+import java.util.*;
+import java.lang.Math;
+
+public class T03 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int stok, tahunterbit;
+        String isbn, judul, penulis, penerbit, formatbukuelektrik, none, kategori, kategorirate, ending, kategoridiskon;
+        double hargabeli, minimummargin, rating;
+
+        kategorirate = "";
+        do {
+            isbn = input.nextLine();
+            if (isbn.equals("---")) {
+            } else {
+                judul = input.nextLine();
+                penulis = input.nextLine();
+                tahunterbit = input.nextInt();
+                penerbit = input.nextLine();
+                formatbukuelektrik = input.nextLine();
+                hargabeli = input.nextDouble();
+                minimummargin = input.nextDouble();
+                if (minimummargin > 0) {
+                    kategoridiskon = "---";
+                } else {
+                    if (minimummargin < -(hargabeli * 0.4)) {
+                        kategoridiskon = "Once in a lifetime";
+                    } else {
+                        if (minimummargin < -(hargabeli * 0.2)) {
+                            kategoridiskon = "Never come twice";
+                        } else {
+                            kategoridiskon = "No regret";
+                        }
+                    }
+                }
+                stok = input.nextInt();
+                rating = input.nextDouble();
+                if (rating >= 4.7) {
+                    kategori = "Best Pick";
+                } else {
+                    if (rating >= 4.5) {
+                        kategori = "Must Read";
+                    } else {
+                        if (rating >= 4.0) {
+                            kategori = "Recommended";
+                        } else {
+                            if (rating >= 3.0) {
+                                kategori = "Average";
+                            } else {
+                                if (rating >= 0 && rating < 3.0) {
+                                    kategori = "Low";
+                                } else {
+                                    kategori = "none";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (kategoridiskon.equals("Once in a lifetime") && kategorirate.equals("Best Pick")) {
+                ending = "The Ultimate Best";
+            } else {
+                ending = "---";
+            }
+            System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbukuelektrik + "|" + hargabeli + "|" + minimummargin + "|" + stok + "|" + rating + "|" + kategori + "|" + kategorirate + "|" + kategoridiskon + "|" + ending);
+        } while (isbn.equals("---"));
+    }
+}
